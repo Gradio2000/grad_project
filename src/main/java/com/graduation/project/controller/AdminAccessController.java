327b009066e794936ab1bb7fb5be502ebe4804fa
Gradio2000 <laskin999@gmail.com>
2021-11-09 22:03:36 +0500

    43 getmapping user/{id} and exception handler

--- src/main/java/com/graduation/project/controller/AdminAccessController.java
+++ src/main/java/com/graduation/project/controller/AdminAccessController.java
@@ -6,6 +6,7 @@ import com.graduation.project.repository.DishRepository;
 import com.graduation.project.repository.UserRepository;
 import com.graduation.project.repository.VoitRepository;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.h2.jdbc.JdbcSQLIntegrityConstraintViolationException;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
@@ -16,7 +17,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
@@ -38,10 +41,10 @@ public class AdminAccessController {
     }
 
     private static final RepresentationModelAssemblerSupport<User, EntityModel<User>> ASSEMBLER =
-            new RepresentationModelAssemblerSupport<>(UserAccessController.class, (Class<EntityModel<User>>) (Class<?>) EntityModel.class) {
+            new RepresentationModelAssemblerSupport<>(AdminAccessController.class, (Class<EntityModel<User>>) (Class<?>) EntityModel.class) {
                 @Override
                 public EntityModel<User> toModel(User user) {
-                    return EntityModel.of(user, linkTo(UserAccessController.class).slash(user.getUserId()).withSelfRel());
+                    return EntityModel.of(user, linkTo(AdminAccessController.class).slash("users").slash(user.getUserId()).withSelfRel());
                 }
             };
 
@@ -65,5 +68,18 @@ public class AdminAccessController {
     }
 
 
+    @GetMapping(value = "/users/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<EntityModel<User>> getUserById(@PathVariable Integer id){
+        return new ResponseEntity<>(ASSEMBLER.toModel(userRepository.getById(id)), HttpStatus.OK);
+    }
+
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(JdbcSQLIntegrityConstraintViolationException.class)
+    public Map<String, String> handleExceptions(JdbcSQLIntegrityConstraintViolationException ex) {
+        Map<String, String> errors = new HashMap<>();
+        errors.put("error", "Restaurant is not present");
+        return errors;
+    }
 
 }
--- src/main/java/com/graduation/project/repository/UserRepository.java
+++ src/main/java/com/graduation/project/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.graduation.project.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.Optional;
 
